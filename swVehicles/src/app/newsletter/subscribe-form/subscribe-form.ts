fb5c3e7c553babb1349e3a3c6131e8a57926245e
DeborahK <deborahk@insteptech.com>
2025-12-01 12:57:20 -0800

    Update to match 'first look' YT video

--- swVehicles/src/app/newsletter/subscribe-form/subscribe-form.ts
+++ swVehicles/src/app/newsletter/subscribe-form/subscribe-form.ts
@@ -1,5 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { form, Field } from '@angular/forms/signals';
+import { Component, computed, effect, signal } from '@angular/core';
+import { form, Field, submit } from '@angular/forms/signals';
 import { initialData, Subscription, subscriptionSchema } from '../subscription';
 
 @Component({
@@ -9,24 +9,46 @@ import { initialData, Subscription, subscriptionSchema } from '../subscription';
   styleUrl: './subscribe-form.css',
 })
 export class SubscribeForm {
-  subscribeMessage = '';
+  subscribeMessage = signal('');
+  errorMessage = signal('');
 
   // Create a form model signal with form fields
   // This represents the form's data structure
   subscribeModel = signal<Subscription>(initialData);
+  fullName = computed(() => `${this.subscribeModel().firstName} ${this.subscribeModel().lastName}`);
+  pageHeader = computed(() => `Subscribe to our Newsletter ${this.fullName()}`);
 
-  // Define a form from the model
+  // Declare a form from the model and logic rules schema
   subscribeForm = form(this.subscribeModel, subscriptionSchema);
-  fullName = computed(() => `${this.subscribeForm.firstName().value()} ${this.subscribeForm.lastName().value()}`);
+
+  cancel() {
+    // Reset form (or navigate to another page)
+    this.resetForm();
+  }
 
   subscribe() {
-    // Display a message
-    this.subscribeMessage = `Thank you for your subscription ${this.fullName()}!`;
-    // Submit to the server
-    console.log('Submitting data to server', this.subscribeForm().value());
-    // Reset the form (or navigate to another page)
-    this.subscribeForm().reset();
-    // Clear the existing data
-    this.subscribeModel.set(initialData);
+    this.subscribeMessage.set('');
+    submit(this.subscribeForm, () =>
+      this.onSubmit());
   }
+
+  async onSubmit() {
+    this.subscribeMessage.set(`Thank you for your subscription ${this.fullName()}!`);
+    // Submit to the server
+    console.log('Submitting data to server:', this.subscribeForm().value());
+    // Reset form (or navigate to another page)
+    this.resetForm();
+  }
+
+  resetForm() {
+    // Reset the form
+    this.subscribeForm().reset();
+    // Reset to the initial model data
+    this.subscribeModel.set(initialData);
+
+  }
+
+  eff = effect(() => {
+    console.log('Email:', this.subscribeModel().email);
+  });
 }
--- swVehicles/src/app/newsletter/subscription.ts
+++ swVehicles/src/app/newsletter/subscription.ts
@@ -1,25 +1,24 @@
-import { email, max, min, required, schema } from "@angular/forms/signals";
+import { email, max, min, minLength, required, schema } from "@angular/forms/signals";
 
 export interface Subscription {
   email: string;
   firstName: string;
   lastName: string;
   yearsAsFan: number;
-  priceAlerts: boolean;
 }
 
 export const initialData = {
   email: '',
   firstName: '',
   lastName: '',
-  yearsAsFan: 0,
-  priceAlerts: false,
+  yearsAsFan: NaN
 };
 
 // Define the validation as part of the model
 export const subscriptionSchema = schema<Subscription>((path) => {
-  required(path.email, { message: 'Email is required to receive our newsletter' });
-  email(path.email, { message: 'Please enter a valid email address' });
+  required(path.email, { message: 'Your email address is required to receive our newsletter' });
+  email(path.email, { message: 'Please enter a a valid email address'});
+  minLength(path.email, 6, {message: 'The email must be at least 6 characters long'});
   min(path.yearsAsFan, 0, { message: 'Years cannot be negative' });
   max(path.yearsAsFan, 100, { message: 'Please enter a valid number of years' });
 })
