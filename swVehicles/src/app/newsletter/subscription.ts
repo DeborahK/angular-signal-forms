e5c75c10e28cfe11de43861dad6fdab1fb94f4d2
DeborahK <deborahk@insteptech.com>
2025-12-02 12:39:03 -0800

    Change path variable name for clarity

--- swVehicles/src/app/newsletter/subscription.ts
+++ swVehicles/src/app/newsletter/subscription.ts
@@ -15,10 +15,10 @@ export const initialData = {
 };
 
 // Define the validation as part of the model
-export const subscriptionSchema = schema<Subscription>((path) => {
-  required(path.email, { message: 'Your email address is required to receive our newsletter' });
-  email(path.email, { message: 'Please enter a a valid email address'});
-  minLength(path.email, 6, {message: 'The email must be at least 6 characters long'});
-  min(path.yearsAsFan, 0, { message: 'Years cannot be negative' });
-  max(path.yearsAsFan, 100, { message: 'Please enter a valid number of years' });
+export const subscriptionSchema = schema<Subscription>((rootPath) => {
+  required(rootPath.email, { message: 'Your email address is required to receive our newsletter' });
+  email(rootPath.email, { message: 'Please enter a a valid email address'});
+  minLength(rootPath.email, 6, {message: 'The email must be at least 6 characters long'});
+  min(rootPath.yearsAsFan, 0, { message: 'Years cannot be negative' });
+  max(rootPath.yearsAsFan, 100, { message: 'Please enter a valid number of years' });
 })
