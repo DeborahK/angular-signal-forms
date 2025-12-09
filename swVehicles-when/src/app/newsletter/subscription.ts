d1b232043b047fb185c62423963db8dd15183021
DeborahK <deborahk@insteptech.com>
2025-12-09 12:24:15 -0800

    Add when and applyWhen to validation

--- swVehicles-when/src/app/newsletter/subscription.ts
+++ swVehicles-when/src/app/newsletter/subscription.ts
@@ -1,4 +1,4 @@
-import { email, max, min, minLength, required, schema } from "@angular/forms/signals";
+import { applyWhen, email, max, min, minLength, required, schema } from "@angular/forms/signals";
 
 export interface Subscription {
   email: string;
@@ -22,10 +22,20 @@ export const initialData: Subscription = {
 
 // Define the validation as part of the model
 export const subscriptionSchema = schema<Subscription>((rootPath) => {
-  required(rootPath.email, { message: 'Your email address is required to receive our newsletter' });
+  required(rootPath.email, {
+    message: 'Your email address is required to receive our newsletter',
+    when: ({ valueOf }) => valueOf(rootPath.sendViaEmail) === true
+  });
   email(rootPath.email, { message: 'Please enter a valid email address' });
-  required(rootPath.phone, { message: 'Your cell phone number is required to receive our newsletter' });
   minLength(rootPath.email, 6, { message: 'The email must be at least 6 characters long' });
+  applyWhen(
+    rootPath.phone,
+    ({ valueOf }) => valueOf(rootPath.sendViaText) === true,
+    (phonePath) => {
+      required(phonePath, { message: 'Your cell phone number is required to receive our newsletter' }),
+      minLength(phonePath, 10, { message: 'Minimum of 10 digits is required' })
+    }
+  ),
   min(rootPath.yearsAsFan, 0, { message: 'Years cannot be negative' });
   max(rootPath.yearsAsFan, 100, { message: 'Please enter a valid number of years' });
 });
